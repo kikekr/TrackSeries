9202b56e1e063729fa1ca9f5c2d89b20b68243bf
Christian Ponte <christian.ponte@udc.es>
2015-11-23 17:02:20 +0000

    Modificado views para usar mi template (prueba)

--- appseries/series/views.py
+++ appseries/series/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return render(request, 'series/NuevaSerie.html')
+	return render(request, 'series/index.html')
