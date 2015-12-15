a9ed1648f4a9bd045a05084620595d5e93642a7e
Christian Ponte <christian.ponte@udc.es>
2015-12-15 16:42:31 +0000

    Añadida entrada para estadisticas

--- appseries/series/urls.py
+++ appseries/series/urls.py
@@ -3,6 +3,7 @@ from series import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^estadisticas/', views.index, name='estadisticas'),
 	url(r'^register/', views.register, name='register'),
 	url(r'^', include('django.contrib.auth.urls')),
 ]
