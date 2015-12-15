86de2520a1abb1e77eb9ad3a0eb7301844acaeaf
Christian Ponte <christian.ponte@udc.es>
2015-12-15 16:43:07 +0000

    Eliminado paquete SecurityMiddleware

--- appseries/appseries/settings.py
+++ appseries/appseries/settings.py
@@ -48,8 +48,9 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
 )
+#'django.middleware.security.SecurityMiddleware',
+
 
 ROOT_URLCONF = 'appseries.urls'
 
