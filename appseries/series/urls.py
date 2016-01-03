6d84e1d285d210cf9f4cff3880e54526d49870c4
kike <enrique.goberna@udc.es>
2016-01-03 13:43:22 +0100

    Añadiendo vista para comprobación de novedades

--- appseries/series/urls.py
+++ appseries/series/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^(\d+)/$', views.serie, name='serie'),
 	url(r'^estadisticas/', views.estadisticas, name='estadisticas'),
 	url(r'^nueva/', views.nuevaSerie, name='nueva'),
+	url(r'^novedades/', views.novedades, name='novedades'),
 	url(r'^nuevaserie/(?P<identifier>\d+)/$', views.addSerie, name='addSerie'),
 	url(r'^register/', views.register, name='register'),
 	url(r'^', include('django.contrib.auth.urls')),
