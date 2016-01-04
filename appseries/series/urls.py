d26629610f00d9ad3fd5769b6966b3c469519559
Christian Ponte <christian.ponte@udc.es>
2016-01-04 08:17:17 +0000

    Cambiada la entrada de estadísticas

--- appseries/series/urls.py
+++ appseries/series/urls.py
@@ -5,10 +5,10 @@ from django.conf.urls.static import static
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
     url(r'^(\d+)/$', views.serie, name='serie'),
-	url(r'^estadisticas/', views.estadisticas, name='estadisticas'),
-	url(r'^nueva/', views.nuevaSerie, name='nueva'),
-	url(r'^novedades/', views.novedades, name='novedades'),
+	url(r'^(\d+)/estadisticas/$', views.estadisticas, name='estadisticas'),
+	url(r'^nueva/$', views.nuevaSerie, name='nueva'),
+	url(r'^novedades/$', views.novedades, name='novedades'),
 	url(r'^nuevaserie/(?P<identifier>\d+)/$', views.addSerie, name='addSerie'),
-	url(r'^register/', views.register, name='register'),
+	url(r'^register/$', views.register, name='register'),
 	url(r'^', include('django.contrib.auth.urls')),
 ]
