8a6ebc31e4f6733896dce2f081d6d972efbb1515
Christian Ponte <christian.ponte@udc.es>
2016-01-27 03:29:06 +0100

    Añadida descarga de torrents, y mostrar las ips a las que se encuentra conectado

--- appseries/series/management/commands/startanalysis.py
+++ appseries/series/management/commands/startanalysis.py
@@ -1,17 +1,24 @@
 # -*- coding: utf-8 -*-
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
+from series.models import Serie
+
 import threading
 from lxml import html
 import requests
 from requests.utils import quote
-
-from series.models import Serie
 from datetime import datetime, timedelta
 from time import sleep
+import os
+import libtorrent as lt
 
 class Command(BaseCommand):
     help = u"Inicia el análisis para el capítulo de la serie indicada"
+
+    tempUrl = "/tmp/.appseries/"
+
+    # Dormir durante 10s hasta próxima actualización
+    sleepTime = 10
 
     def getTorrentsForEpisode(self, tvshow, season, episode, count):
         # Url de búsqueda en kat.cr
@@ -25,6 +32,13 @@ class Command(BaseCommand):
         # Devolvemos los enlaces
         return map(lambda x: x.attrib['href'], links[:count])
 
+    def getTorrentFileAsString(self, url):
+        # Necesario cambiar el User-Agent, sino el servidor cierra la conexión TCP
+        headers = {"User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36"}
+        if url.startswith("/"):
+            url = "http:" + url
+        return requests.get(url, headers=headers).content
+
     def analyze(self, serieId, temporada, capitulo):
         try:
             serie = Serie.objects.get(id=int(serieId))
@@ -32,30 +46,43 @@ class Command(BaseCommand):
             self.stdout.write(u"No existe serie con id %i\n" % (int(serie)))
             return
 
+        # Falta comprobar si el directorio temporal existe, y crearlo en caso negativo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
         # Set de urls siendo actualmente descargadas
         urls = set()
-        # Lista de las transferencias activas
-        activeTransfers = []
         # Momento de finalización
         deadline = datetime.now() + timedelta(0, 0, 0, 0, 0, serie.tiempoAnalisis)
 
+        # Inicialización de la sesión de
+        ses = lt.session()
+
         while (deadline - datetime.now()).total_seconds()>0:
             # Comprobar si hay más urls disponibles en caso de no disponer de suficientes
-            if len(urls)<urlLimit:
+            if len(urls)<serie.numeroTorrents:
                 newUrls = set(self.getTorrentsForEpisode(serie.nombre, int(temporada), int(capitulo), serie.numeroTorrents)) - urls
                 for u in newUrls:
                     # Iniciar descarga para cada url
+                    e = lt.bdecode(self.getTorrentFileAsString(u))
+                    info = lt.torrent_info(e)
+                    params = { "save_path": "/tmp/.appseries/", "storage_mode": lt.storage_mode_t.storage_mode_sparse, "ti": info }
+                    ses.add_torrent(params)
+                    # Tal vez establecer un limite de bajada y subida?
+
+                    # Añadir la url de la descarga a la lista
                     urls.add(u)
 
             # Para cada descarga comprobar a que peers se encuentra conectado
+            for h in ses.get_torrents():
+                for peer in h.get_peer_info():
+                    # Debug
+                    ip, port = peer.ip
+                    self.stdout.write(ip)
 
-            # Debug
-            self.stdout.write(u"Segundos restantes: %i\n" % (tiempoFin - datetime.now()).total_seconds())
+            sleep(sleepTime)
 
-            # Dormir durante 10s hasta próxima actualización
-            sleep(10)
-
-        # Cerrar todas las conexiones activas
+        # Cerrar todas las conexiones activas !!!!!!!!!!!!!!!!! Sin debugear si funciona
+        for h in ses.get_torrents():
+            ses.remove_torrent(h)
 
     def handle(self, *args, **options):
         if len(args)==3:
